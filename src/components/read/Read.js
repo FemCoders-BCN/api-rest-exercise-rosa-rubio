82d3ef76f0655e1a17c00d80e16b7d6cf1518528
rosa-rubio <rosamrubio@gmail.com>
2023-08-16 23:51:44 +0200

    refactoring the CRUD

--- src/components/read/Read.js
+++ src/components/read/Read.js
@@ -1,25 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { FavoriteService } from '../../services/FavoriteService';
 import Update from '../../components/update/Update';
 
-const Read = () => {
-  const [pictures, setPictures] = useState([]);
+const Read = ({picture, onDelete }) => {
+
   const [selectedPicture, setSelectedPicture] = useState(null);
 
-  useEffect(() => {
-    const service = FavoriteService();
-    service.getAll()
-      .then(response => setPictures(response.data))
-      .catch(error => console.log(error));
-  }, []);
-
   const handleOnClickDelete = (pictureId) => {
+    
     const service = FavoriteService();
     service.deletePicture(pictureId)
-      .then(response => console.log(response))
+      .then(response => {
+        onDelete(pictureId);
+      })
       .catch(error => console.log(error));
-
-      setPictures(prevPictures => prevPictures.filter(picture => picture.id !== pictureId));  
   };
 
   const handleOnClickEdit = (pictureId, authorName, imageURL) => {
@@ -32,29 +26,21 @@ const Read = () => {
 
   return (
     <div>
-      <h1>My pictures</h1>
-      <div className="pictures-container">
-        {pictures.map((picture) => (
-          <div key={picture.id}>
-            <div>ID: {picture.id}</div>
-            <div>Author: {picture.author}</div>
-            <img src={picture.download_url} alt={picture.author} />
-            <div>
-              <button onClick={() => handleOnClickDelete(picture.id)}>Eliminar</button>
-              <button onClick={() => handleOnClickEdit(picture.id, picture.author, picture.download_url)}>Editar</button>
-            </div>
-          </div>
-        ))}
+      <p>Autor: {picture.author}</p>
+      <img src={picture.download_url} alt="" />
+      <div>
+        <button onClick={() => handleOnClickDelete(picture.id)}>Eliminar</button>
+        <button onClick={() => handleOnClickEdit(picture.id, picture.author, picture.download_url)}>Editar</button>
       </div>
       {selectedPicture && (
         <Update
-          id={selectedPicture.id}
-          name={selectedPicture.name}
-          url={selectedPicture.url}
-        />
-      )}
+         id={selectedPicture.id}
+         name={selectedPicture.name}
+         url={selectedPicture.url}
+         />
+        )}
     </div>
-  );
+  )
 };
 
 export default Read;
